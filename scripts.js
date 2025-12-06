6536a3ddd3aba6513c47dc498c4ec722b8caaccf
Keago99 <keatabde@gmail.com>
2025-12-06 13:33:14 +0200

    Add outline for visual aesthetic
    Add color scheme and font idea comment,
    Add 3x3 grid with sizing

---
+++ scripts.js
@@ -0,0 +1,20 @@
+
+
+const Gameboard = function (){
+    gameBoard = [];
+
+    //
+
+}
+
+const Player = function (Name){
+
+}
+
+const gameController = function (){
+    
+}
+
+// new game
+// clear board
+// while loop
