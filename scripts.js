659235a1664144ad26d0c3c6cf6d9b436811aac1
Keago99 <keatabde@gmail.com>
2025-12-09 21:31:09 +0200

    Add JS logic
    Add JS logic to import statements, begin to test
    click events on squares.

--- scripts.js
+++ scripts.js
@@ -1,20 +1,131 @@
 
 
-const Gameboard = function (){
-    gameBoard = [];
+// IIFE since there is only one gameboard
+const GameboardController = (function () {
+    const gameBoard = [null, null, null, null, null, null, null, null, null];
 
-    //
+    const getBoard = () => gameBoard;
 
+    const markBoardArray = (index) => {
+        if (gameBoard[index] === null) {
+            if (currentPlayerTurn === 1){
+                gameBoard[index] = "X";
+            }
+            else{
+                gameBoard[index] = "O";
+            }
+        }
+        else{
+            alert("This cell was already played!");
+        }
+    };
+
+    const checkWin =  (gameBoard) => {
+        const winPatterns = [
+            [0, 1, 2], // Top row
+            [3, 4, 5], // Middle row
+            [6, 7, 8], // Bottom row
+            [0, 3, 6], // Left column
+            [1, 4, 7], // Middle column
+            [2, 5, 8], // Right column
+            [0, 4, 8], // Diagonal \
+            [2, 4, 6]  // Diagonal /
+            ];
+
+        for (const pattern of winPatterns){
+            const[a,b,c] = pattern;
+
+            if (gameBoard[a] &&
+                gameBoard[a] === gameBoard[b] &&
+                gameBoard[a] === gameBoard[c]) {
+                    return{
+                        winner: gameBoard[a], // the winning board symbol (X or O)
+                        winningCells: pattern // the positions of [a, b, c]
+                    };
+                }
+        }
+
+        if (!gameBoard.includes(null)){
+            return {winner: "draw"}; // returns an object containing winner: draw
+        }
+
+        return null; // This is the final, if no winner or draw is found we continue with null being returned
+    }
+
+    // Big composite function, this should run through the entire chain of events
+
+    return { markBoardArray, checkWin, getBoard  };
+})();
+
+// Player factory for two players
+const Player = function (name) {
+
+    this.name = name;
+    let score = 0;
+    const increaseScore = () => score++;
+    const getScore = () => score;
+
+    const getName = () => name;
+    const setName = (newName) => { name = newName };
+
+    let symbol = null;
+    const getSymbol = () => symbol;
+    const setSymbol = (newSymbol) => { symbol = newSymbol };
+
+    return { setName, getName, increaseScore, getScore, setSymbol, getSymbol };
 }
 
-const Player = function (Name){
+// updating the visualls.
+const displayController = (function (){
+    const cells = document.querySelectorAll(".cell");
 
-}
+    const addClickers = () => {
+        cells.forEach(cell => {
+            cell.addEventListener("click", function(){
+                gameController.handleCellClick(this.datacCellIndex);
+            });
+        });
+    };
+    return { addClickers };
+})();
 
-const gameController = function (){
-    
-}
+// IIFE since there is only one game controller
+const gameController = (function () {
 
-// new game
-// clear board
-// while loop
+    let startingPlayer = Math.floor(Math.random() * 2) + 1;
+    let currentPlayerTurn = startingPlayer;
+
+    const getCurrentPlayerTurn = () => currentPlayerTurn;
+
+
+    // For switching the turns after each move
+    const switchTurn = () => {
+        if(currentPlayerTurn === 1){
+            currentPlayerTurn = 2;
+        }
+        else{
+            currentPlayerTurn = 1;
+        }
+    };
+
+    // ingition switch
+    const startGame = () => {
+        player1 = new Player("Keagan");
+        player2 = new Player("Chun-li");
+        player1.setSymbol("X");
+        player2.setSymbol("Y");
+        displayController.addClickers();
+    };
+
+    const handleCellClick = (index) => {
+        console.log(this);
+        /* depening on the turn will mark the cell
+         then will check for win, if return {winner:} object we conclude the game
+         if return null the game continues by swapping the current turn by changing the currentTurn variable using the switchTurn Function
+        */
+    };
+
+    return { getCurrentPlayerTurn, handleCellClick, switchTurn, startGame };
+})();
+
+gameController.startGame();
